29ae4a8655a95cbb53a2e4c49da939e1374d0320
xiaowei <119865@shfang.net>
2018-04-18 11:15:10 +0800

    change build name

--- config/index.js
+++ config/index.js
@@ -50,10 +50,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../partyBuildingCloudAdmin/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../partyBuildingCloudAdmin'),
     assetsSubDirectory: 'static',
 
     // you can set by youself according to actual condition
