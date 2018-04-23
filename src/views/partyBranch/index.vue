8c0e17b086b67b9da3d05a619b48318bcc009fca
xiaowei <119865@shfang.net>
2018-04-23 16:14:38 +0800

    partyBranch UI 0.2

--- src/views/partyBranch/index.vue
+++ src/views/partyBranch/index.vue
@@ -1,9 +1,15 @@
 <template>
 <div class='app-container'>
-  <el-row type="flex" justify="center">
-        <el-button class="create_button" @click.native.prevent='createRow()' type="primary" icon="el-icon-edit"></el-button>
-  </el-row>
-  <el-table v-if="tableData.length" class='houses-table' :data='tableData' stripe border @selection-change="handleSelectionChange" highlight-current-row @current-change="handleCurrentChange">
+  <div class="top_bar">
+    <el-button class="create_button" @click.native.prevent='createRow()' type='primary' size='mini'>添加</el-button>
+    <div class="search_address_page">
+      <form class="search_form">
+          <input type="search" name="search" placeholder="请输入党支部名称" v-model="searchValue">
+          <el-button @click.prevent="searchPartyBranch()" type='primary' size='mini'>搜索</el-button>
+      </form>
+    </div>
+  </div>
+  <el-table v-if="tableData.length" class='party_branch_table' :data='tableData' stripe border @selection-change="handleSelectionChange" highlight-current-row @current-change="handleCurrentChange">
         <!-- <el-table-column prop='ID' label='ID' width='100'></el-table-column> -->
         <!-- <el-table-column prop='resourceType' label='资源ID' width='180' :filters='resourceTypeFilersData' :filter-method='filterHandler'></el-table-column> -->
         <el-table-column prop='title' label='名称' width='180'></el-table-column>
@@ -16,13 +22,13 @@
         </el-table-column>
         <el-table-column fixed='right' label='操作' width='220'>
           <template slot-scope='scope'>
-            <el-button @click.native.prevent='detailRow(scope.$index, scope.row, scope.column, tableData)' type='info' size='mini'>详情</el-button>
+            <el-button @click.native.prevent='detailRow(scope.$index, scope.row, scope.column, tableData)' type='primary' size='mini'>详情</el-button>
             <el-button @click.native.prevent='editRow(scope.$index, scope.row, scope.column, tableData)' type='primary' size='mini'>编辑</el-button>
-            <el-button @click.native.prevent='deleteRow(scope.$index, scope.row, scope.column, tableData)' type='primary' size='mini'>删除</el-button>
+            <el-button @click.native.prevent='deleteRow(scope.$index, scope.row, scope.column, tableData)' type='danger' size='mini'>删除</el-button>
           </template>
         </el-table-column>
     </el-table>
-    <div v-else >没有任何党支部数据, 请添加!</div>
+    <div class='no_data_div' v-else >无党支部数据!</div>
     <el-dialog title="删除提示" :visible.sync="dialogVisible" width="30%">
       <span>是否删除 {{multipleSelection.length}}项数据?</span>
       <span slot="footer" class="dialog-footer">
@@ -35,12 +41,14 @@
 </template>
 
  <script>
-import { getHouses } from '@/api/houses'
+import { getPartyBranch } from '@/api/partyBranch'
 
 export default {
   data() {
     return {
+      searchValue: '',
       tableData: [],
+      allBranchData: [],
       resourceTypeFilersData: [
         {
           text: 'V01',
@@ -64,26 +72,39 @@ export default {
       currentRow: null
     }
   },
-  created() {
-    console.log('created')
+  mounted() {
+    console.log('mounted')
     this.fetchData()
   },
   methods: {
     fetchData() {
-      getHouses().then(response => {
+      getPartyBranch().then(response => {
         this.tableData = response.data.lists
+        this.allBranchData = response.data.lists
       })
     },
     handleCurrentChange(val) {
       console.log(val)
       this.currentRow = val
       // TODO跳转 router
-      this.$router.push({ path: '/houses/detail/' + this.currentRow.ID })
+      this.$router.push({ path: '/branch/detail/' + this.currentRow.ID })
     },
     isShowDeleteDialog() {
       console.log('isShowDeleteDialog')
       if (this.multipleSelection.length !== 0) {
         this.dialogVisible = true
+      }
+    },
+    searchPartyBranch(){
+      console.log('searchPartyBranch, searchValue is ' + this.searchValue)
+      if(this.searchValue === '') {
+        this.tableData = this.allBranchData
+      }
+      else {
+        this.tableData = this.allBranchData.filter(branch => {
+            console.log(branch.title)
+            return branch.title.indexOf(this.searchValue) !== -1
+        })
       }
     },
     handleSelectionChange(val) {
@@ -96,7 +117,7 @@ export default {
     createRow() {
       console.log('create')
       // TODO: 跳转到创建页面
-      this.$router.push({ path: '/houses/create' })
+      this.$router.push({ path: '/branch/create' })
     },
     editRow(index, row, column, data) {
       console.log('edit')
@@ -129,15 +150,38 @@ export default {
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .app-container {
-  .el-row {
+  width: 13.5rem;
+  .top_bar {
     background: #e5e9f2;
     height: 0.5rem;
+    font-size: 0.2rem;
     .create_button {
+      margin-top: 0.05rem;
+      margin-left: 0.2rem;
+      float: left;
+      width: 1.5rem;
+      height: 0.4rem;
+    }
+    .search_address_page{
+      margin-left: 1.8rem;
       height: 0.5rem;
+      .search_form {
+        font-size: 0.2rem;
+        button{
+          margin-top: 0.05rem;
+          width: 1.5rem;  
+          height: 0.4rem;
+        }
+        input{
+          margin-top: 0.05rem;
+          width: 9.5rem;
+          height: 0.4rem;
+        }
+      }
     }
   }
-  // .houses-table {
-  //   top: 40px;
-  // }
+  .no_data_div{
+    font-size: 1rem
+  }
 }
 </style>
